41c098c8883ae0f76fcc3591ccefecd2bbfea90e
AwondoFru <awondo.fru@yahoo.com>
2019-07-03 11:29:22 +0100

    Mails controller developed

--- src/main/java/controllers/MailsController.java
+++ src/main/java/controllers/MailsController.java
@@ -6,8 +6,14 @@
  */
 package controllers;
 
+import entities.Mails;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 import javax.inject.Named;
+import sessions.MailsFacade;
 
 /**
  *
@@ -15,5 +21,95 @@ import javax.inject.Named;
  */
 @Named
 public class MailsController implements Serializable  {
+    
+    private Mails mail;
+    private List<Mails> mailList;
+    private Integer numOfMails;
+    
+    @Inject
+    private MailsFacade mailOperation;
+    
+    public MailsController(){
+        
+    }
+    
+    public void intializeLists(){
+        mailList.clear();
+        mailList.addAll(mailOperation.findAll());
+        numOfMails = mailList.size();
+    }
+    
+    @PostConstruct
+    public void init(){
+        mailList = new ArrayList<>();
+        mail = new Mails();
+    }
+    
+    /**
+     * Saves a mail using the current persistence unit
+     */
+    public void save(){
+        try{
+            mailOperation.create(mail);
+        }catch(Exception ex){
+            
+        }
+    }
+    
+    /**
+     * Edits a mail using the current persistence unit
+     */
+    
+    public void edit(){
+        try{
+            mailOperation.edit(mail);
+        }catch(Exception ex){
+            
+        }
+    }
+    
+    /**
+     * Deletes a mail using the current persistence unit
+     */
+    public void delete(){
+        try{
+            mailOperation.remove(mail);
+        }catch(Exception ex){
+            
+        }
+    }
+
+    public Mails getMail() {
+        return mail;
+    }
+
+    public void setMail(Mails mail) {
+        this.mail = mail;
+    }
+
+    public List<Mails> getMailList() {
+        return mailList;
+    }
+
+    public void setMailList(List<Mails> mailList) {
+        this.mailList = mailList;
+    }
+
+    public MailsFacade getMailOperation() {
+        return mailOperation;
+    }
+
+    public void setMailOperation(MailsFacade mailOperation) {
+        this.mailOperation = mailOperation;
+    }
+
+    public Integer getNumOfMails() {
+        return numOfMails;
+    }
+
+    public void setNumOfMails(Integer numOfMails) {
+        this.numOfMails = numOfMails;
+    }
+    
     
 }
